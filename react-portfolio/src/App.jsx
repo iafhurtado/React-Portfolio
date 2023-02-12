47a2fce46dab333cc54e8e70af019e83d76237be
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-12 21:29:52 +0100

    got the testimonials and faqs done

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './sections/header/Header';
 import About from './sections/about/About';
 import Services from './sections/services/Services';
 import Portfolio from './sections/portfolio/Portfolio';
+import Testimonials from './sections/testimonials/Testimonials';
 import FAQs from './sections/faqs/FAQs';
 import Contact from './sections/contact/Contact';
 import Footer from './sections/footer/Footer';
@@ -18,6 +19,7 @@ const App = () => {
       <About />
       <Services />
       <Portfolio />
+      <Testimonials />
       <FAQs />
       <Contact />
       <Footer />
