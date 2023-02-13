c15831ed0ef21327204b7c3ae0a2918dd4ff66be
JaiBo <65864145+iafhurtado@users.noreply.github.com>
2023-02-13 11:55:22 +0100

    finishing touches done on Floating Navbar

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -11,13 +11,47 @@ import Footer from './sections/footer/Footer';
 import FloatingNav from './sections/floating-nav/FloatingNav';
 import Theme from './theme/Theme';
 import { useThemeContext } from './context/theme-context';
+import {useRef, useState, useEffect} from 'react';
 import './App.css';
 
 const App = () => {
   const {themeState} = useThemeContext();
 
+  const mainRef = useRef();
+  const [showFloatingNav, setShowFloatingNav] = useState(true);
+  const [siteYPostion, setSiteYPosition] = useState(0)
+
+  const showFloatingNavHandler = () => {
+    setShowFloatingNav(true);
+  }
+
+  const hideFloatingNavHandler = () => {
+    setShowFloatingNav(false);
+  }
+
+  // check if floating nav should be shown or hidden
+  const floatingNavToggleHandler = () => {
+    // check if we scrolled up or down at least 20px
+    if(siteYPostion < (mainRef?.current?.getBoundingClientRect().y - 20) || siteYPostion > (mainRef?.current?.getBoundingClientRect().y + 20)) {
+      showFloatingNavHandler();
+    } else {
+      hideFloatingNavHandler();
+    }
+
+    setSiteYPosition(mainRef?.current?.getBoundingClientRect().y);
+  }
+
+  useEffect(() => {
+    const checkYPosition = setInterval(floatingNavToggleHandler, 2000);
+
+    // cleanup function
+    return () => clearInterval(checkYPosition);
+  }, [siteYPostion])
+
+  
+
   return (
-    <main className={`${themeState.primary} ${themeState.background}`}>
+    <main className={`${themeState.primary} ${themeState.background}`} ref={mainRef}>
         <NavBar/>
         <Header/>
         <About/>
@@ -28,7 +62,7 @@ const App = () => {
         <Contact/>
         <Footer/>
         <Theme/>
-        <FloatingNav/>
+        {showFloatingNav && <FloatingNav/>}
     </main>
   )
 }
